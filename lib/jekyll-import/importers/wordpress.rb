e187872281e66d6b454098c14d56877d667dd1d0
Jan Kunzmann <jan@wildehilde.net>
2016-03-05 11:55:15 +0100

    WordPress: Refactored code flow
    * Large processing method broken into smaller methods
    * Ruby control structures

--- lib/jekyll-import/importers/wordpress.rb
+++ lib/jekyll-import/importers/wordpress.rb
@@ -73,23 +73,7 @@ module JekyllImport
       #                   status. Default: [:publish].
       #
       def self.process(opts)
-        options = {
-          :user           => opts.fetch('user', ''),
-          :pass           => opts.fetch('password', ''),
-          :host           => opts.fetch('host', 'localhost'),
-          :socket         => opts.fetch('socket', nil),
-          :dbname         => opts.fetch('dbname', ''),
-          :table_prefix   => opts.fetch('table_prefix', 'wp_'),
-          :site_prefix    => opts.fetch('site_prefix', nil),
-          :clean_entities => opts.fetch('clean_entities', true),
-          :comments       => opts.fetch('comments', true),
-          :categories     => opts.fetch('categories', true),
-          :tags           => opts.fetch('tags', true),
-          :more_excerpt   => opts.fetch('more_excerpt', true),
-          :more_anchor    => opts.fetch('more_anchor', true),
-          :extension      => opts.fetch('extension', 'html'),
-          :status         => opts.fetch('status', ['publish']).map(&:to_sym) # :draft, :private, :revision
-        }
+        options = parse_options opts
 
         if options[:clean_entities]
           begin
@@ -107,90 +91,89 @@ module JekyllImport
         db = Sequel.mysql2(options[:dbname], :user => options[:user], :password => options[:pass],
                           :socket => options[:socket], :host => options[:host], :encoding => 'utf8')
 
-        px = options[:table_prefix]
-        sx = options[:site_prefix]
-
-        page_name_list = {}
-
-        page_name_query = "
-           SELECT
-             posts.ID            AS `id`,
-             posts.post_title    AS `title`,
-             posts.post_name     AS `slug`,
-             posts.post_parent   AS `parent`
-           FROM #{px}#{sx}posts AS `posts`
-           WHERE posts.post_type = 'page'"
-
-        db[page_name_query].each do |page|
-          if !page[:slug] or page[:slug].empty?
-            page[:slug] = sluggify(page[:title])
-          end
-          page_name_list[ page[:id] ] = {
-            :slug   => page[:slug],
-            :parent => page[:parent]
-          }
-        end
-
-        posts_query = "
-           SELECT
-             posts.ID            AS `id`,
-             posts.guid          AS `guid`,
-             posts.post_type     AS `type`,
-             posts.post_status   AS `status`,
-             posts.post_title    AS `title`,
-             posts.post_name     AS `slug`,
-             posts.post_date     AS `date`,
-             posts.post_date_gmt AS `date_gmt`,
-             posts.post_content  AS `content`,
-             posts.post_excerpt  AS `excerpt`,
-             posts.comment_count AS `comment_count`,
-             users.display_name  AS `author`,
-             users.user_login    AS `author_login`,
-             users.user_email    AS `author_email`,
-             users.user_url      AS `author_url`
-           FROM #{px}#{sx}posts AS `posts`
-             LEFT JOIN #{px}users AS `users`
-               ON posts.post_author = users.ID"
-
-        if options[:status] and not options[:status].empty?
-          status = options[:status][0]
-          posts_query << "
-           WHERE posts.post_status = '#{status.to_s}'"
-          options[:status][1..-1].each do |status|
-            posts_query << " OR
-             posts.post_status = '#{status.to_s}'"
-          end
-        end
-
-        db[posts_query].each do |post|
-          process_post(post, db, options, page_name_list)
-        end
+        post_list = process_posts db, options
+        generate_filenames post_list, db, options
+        write_posts post_list
       end
 
 
-      def self.process_post(post, db, options, page_name_list)
+      def self.parse_options(opts)
+        {
+          :user           => opts.fetch('user', ''),
+          :pass           => opts.fetch('password', ''),
+          :host           => opts.fetch('host', 'localhost'),
+          :socket         => opts.fetch('socket', nil),
+          :dbname         => opts.fetch('dbname', ''),
+          :table_prefix   => opts.fetch('table_prefix', 'wp_'),
+          :site_prefix    => opts.fetch('site_prefix', nil),
+          :clean_entities => opts.fetch('clean_entities', true),
+          :comments       => opts.fetch('comments', true),
+          :categories     => opts.fetch('categories', true),
+          :tags           => opts.fetch('tags', true),
+          :more_excerpt   => opts.fetch('more_excerpt', true),
+          :more_anchor    => opts.fetch('more_anchor', true),
+          :extension      => opts.fetch('extension', 'html'),
+          :status         => opts.fetch('status', ['publish']).map(&:to_sym) # :draft, :private, :revision
+        }
+      end
+
+
+      def self.process_posts(db, options)
         px = options[:table_prefix]
         sx = options[:site_prefix]
-        extension = options[:extension]
+
+        query = "
+          SELECT
+            posts.ID            AS `id`,
+            posts.guid          AS `guid`,
+            posts.post_type     AS `type`,
+            posts.post_status   AS `status`,
+            posts.post_title    AS `title`,
+            posts.post_name     AS `slug`,
+            posts.post_date     AS `date`,
+            posts.post_date_gmt AS `date_gmt`,
+            posts.post_content  AS `content`,
+            posts.post_excerpt  AS `excerpt`,
+            posts.comment_count AS `comment_count`,
+            users.display_name  AS `author`,
+            users.user_login    AS `author_login`,
+            users.user_email    AS `author_email`,
+            users.user_url      AS `author_url`
+          FROM #{px}#{sx}posts AS `posts`
+            LEFT JOIN #{px}users AS `users`
+              ON posts.post_author = users.ID"
+
+        if options[:status] and not options[:status].empty?
+          status_where = options[:status].map do |s|
+            "posts.post_status = '#{s.to_s}'"
+          end.join(" OR ")
+          query << " WHERE #{status_where}"
+        end
+
+        posts = []
+
+        db[query].each do |post|
+          posts << process_post(post, db, options)
+        end
+
+        posts
+      end
+
+
+      def self.process_post(post, db, options)
+        px = options[:table_prefix]
+        sx = options[:site_prefix]
 
         title = post[:title]
-        if options[:clean_entities]
-          title = clean_entities(title)
-        end
+        title = clean_entities(title) if options[:clean_entities]
 
         slug = post[:slug]
-        if !slug or slug.empty?
-          slug = sluggify(title)
-        end
+        slug = sluggify(title) if !slug or slug.empty?
+
+        content = post[:content].to_s
+        content = clean_entities(content) if options[:clean_entities]
 
         date = post[:date] || Time.now
-        name = "%02d-%02d-%02d-%s.%s" % [date.year, date.month, date.day,
-                                         slug, extension]
-        content = post[:content].to_s
-        if options[:clean_entities]
-          content = clean_entities(content)
-        end
-
         excerpt = post[:excerpt].to_s
 
         more_index = content.index(/<!-- *more *-->/)
@@ -203,95 +186,26 @@ module JekyllImport
           if options[:more_anchor]
             more_link = "more"
             content.sub!(/<!-- *more *-->/,
-                         "<a id=\"more\"></a>" +
-                         "<a id=\"more-#{post[:id]}\"></a>")
+                         %{<a id="more"></a>} +
+                         %{<a id="more-#{post[:id]}"></a>})
           end
         end
 
-        categories = []
-        tags = []
-
-        if options[:categories] or options[:tags]
-
-          cquery =
-            "SELECT
-               terms.name AS `name`,
-               ttax.taxonomy AS `type`
-             FROM
-               #{px}#{sx}terms AS `terms`,
-               #{px}#{sx}term_relationships AS `trels`,
-               #{px}#{sx}term_taxonomy AS `ttax`
-             WHERE
-               trels.object_id = '#{post[:id]}' AND
-               trels.term_taxonomy_id = ttax.term_taxonomy_id AND
-               terms.term_id = ttax.term_id"
-
-          db[cquery].each do |term|
-            if options[:categories] and term[:type] == "category"
-              if options[:clean_entities]
-                categories << clean_entities(term[:name])
-              else
-                categories << term[:name]
-              end
-            elsif options[:tags] and term[:type] == "post_tag"
-              if options[:clean_entities]
-                tags << clean_entities(term[:name])
-              else
-                tags << term[:name]
-              end
-            end
-          end
+        categories, tags = if options[:categories] or options[:tags]
+          process_taxonomy post[:id], db, options
+        else
+          [[], []]
         end
 
-        comments = []
-
-        if options[:comments] and post[:comment_count].to_i > 0
-          cquery =
-            "SELECT
-               comment_ID           AS `id`,
-               comment_author       AS `author`,
-               comment_author_email AS `author_email`,
-               comment_author_url   AS `author_url`,
-               comment_date         AS `date`,
-               comment_date_gmt     AS `date_gmt`,
-               comment_content      AS `content`
-             FROM #{px}#{sx}comments
-             WHERE
-               comment_post_ID = '#{post[:id]}' AND
-               comment_approved != 'spam'"
-
-
-          db[cquery].each do |comment|
-
-            comcontent = comment[:content].to_s
-            if comcontent.respond_to?(:force_encoding)
-              comcontent.force_encoding("UTF-8")
-            end
-            if options[:clean_entities]
-              comcontent = clean_entities(comcontent)
-            end
-            comauthor = comment[:author].to_s
-            if options[:clean_entities]
-              comauthor = clean_entities(comauthor)
-            end
-
-            comments << {
-              'id'           => comment[:id].to_i,
-              'author'       => comauthor,
-              'author_email' => comment[:author_email].to_s,
-              'author_url'   => comment[:author_url].to_s,
-              'date'         => comment[:date].to_s,
-              'date_gmt'     => comment[:date_gmt].to_s,
-              'content'      => comcontent,
-            }
-          end
-
-          comments.sort!{ |a,b| a['id'] <=> b['id'] }
+        comments = if options[:comments] and post[:comment_count].to_i > 0
+          process_comments post[:id], db, options
+        else
+          []
         end
 
         # Get the relevant fields as a hash, delete empty fields and
         # convert to YAML for the header.
-        data = {
+        header = {
           'layout'        => post[:type].to_s,
           'status'        => post[:status].to_s,
           'published'     => post[:status].to_s == 'draft' ? nil : (post[:status].to_s == 'publish'),
@@ -312,32 +226,172 @@ module JekyllImport
           'date'          => date.to_s,
           'date_gmt'      => post[:date_gmt].to_s,
           'categories'    => options[:categories] ? categories : nil,
-          'tags'          => options[:tags] ? tags : nil,
-          'comments'      => options[:comments] ? comments : nil,
-        }.delete_if { |k,v| v.nil? || v == '' }.to_yaml
+          'tags'          => options[:tags]       ? tags       : nil,
+          'comments'      => options[:comments]   ? comments   : nil,
+        }.delete_if { |k,v| v.nil? || v == '' }
 
-        if post[:type] == 'page'
-          filename = page_path(post[:id], page_name_list) + "index.#{extension}"
-          FileUtils.mkdir_p(File.dirname(filename))
-        elsif post[:status] == 'draft'
-          filename = "_drafts/#{slug}.md"
-        else
-          filename = "_posts/#{name}"
+        {
+          :slug        => slug,
+          :date        => date,
+          :frontmatter => header,
+          :content     => content,
+          :raw         => post,
+        }
+      end
+
+
+      def self.process_taxonomy(post_id, db, options)
+        px = options[:table_prefix]
+        sx = options[:site_prefix]
+
+        categories = []
+        tags = []
+
+        query = "
+          SELECT
+            terms.name AS `name`,
+            ttax.taxonomy AS `type`
+          FROM
+            #{px}#{sx}terms AS `terms`,
+            #{px}#{sx}term_relationships AS `trels`,
+            #{px}#{sx}term_taxonomy AS `ttax`
+          WHERE
+            trels.object_id = '#{post_id}' AND
+            trels.term_taxonomy_id = ttax.term_taxonomy_id AND
+            terms.term_id = ttax.term_id"
+
+        db[query].each do |term|
+          if options[:categories] and term[:type] == "category"
+            if options[:clean_entities]
+              categories << clean_entities(term[:name])
+            else
+              categories << term[:name]
+            end
+          elsif options[:tags] and term[:type] == "post_tag"
+            if options[:clean_entities]
+              tags << clean_entities(term[:name])
+            else
+              tags << term[:name]
+            end
+          end
         end
 
-        # Write out the data and content to file
-        File.open(filename, "w") do |f|
-          f.puts data
-          f.puts "---"
-          f.puts Util.wpautop(content)
+        [categories, tags]
+      end
+
+
+      def self.process_comments(post_id, db, options)
+        px = options[:table_prefix]
+        sx = options[:site_prefix]
+
+        comments = []
+
+        query = "
+          SELECT
+            comment_ID           AS `id`,
+            comment_author       AS `author`,
+            comment_author_email AS `author_email`,
+            comment_author_url   AS `author_url`,
+            comment_date         AS `date`,
+            comment_date_gmt     AS `date_gmt`,
+            comment_content      AS `content`
+          FROM #{px}#{sx}comments
+          WHERE
+            comment_post_ID = '#{post_id}' AND
+            comment_approved != 'spam'"
+
+        db[query].each do |comment|
+
+          comcontent = comment[:content].to_s
+          comcontent.force_encoding("UTF-8") if comcontent.respond_to?(:force_encoding)
+          comcontent = clean_entities(comcontent) if options[:clean_entities]
+
+          comauthor = comment[:author].to_s
+          comauthor = clean_entities(comauthor) if options[:clean_entities]
+
+          comments << {
+            'id'           => comment[:id].to_i,
+            'author'       => comauthor,
+            'author_email' => comment[:author_email].to_s,
+            'author_url'   => comment[:author_url].to_s,
+            'date'         => comment[:date].to_s,
+            'date_gmt'     => comment[:date_gmt].to_s,
+            'content'      => comcontent,
+          }
+        end
+
+        comments.sort!{ |a,b| a['id'] <=> b['id'] }
+      end
+
+
+      def self.generate_filenames(post_list, db, options)
+        page_name_list = read_page_names db, options
+
+        post_list.each do |post|
+          raw = post[:raw]
+
+          post[:filename] = if raw[:type] == 'page'
+            page_path(raw[:id], page_name_list) + "index.#{options[:extension]}"
+          elsif raw[:status] == 'draft'
+            "_drafts/#{post[:slug]}.md"
+          else
+            date = post[:date]
+            "_posts/%02d-%02d-%02d-%s.%s" %
+              [date.year, date.month, date.day, post[:slug], options[:extension]]
+          end
+
         end
       end
 
 
-      def self.clean_entities( text )
-        if text.respond_to?(:force_encoding)
-          text.force_encoding("UTF-8")
+      def self.read_page_names(db, options)
+        px = options[:table_prefix]
+        sx = options[:site_prefix]
+
+        page_name_list = {}
+
+        query = "
+          SELECT
+            posts.ID            AS `id`,
+            posts.post_title    AS `title`,
+            posts.post_name     AS `slug`,
+            posts.post_parent   AS `parent`
+          FROM #{px}#{sx}posts AS `posts`
+          WHERE posts.post_type = 'page'"
+
+        db[query].each do |page|
+          if !page[:slug] or page[:slug].empty?
+            page[:slug] = sluggify(page[:title])
+          end
+          page_name_list[ page[:id] ] = {
+            :slug   => page[:slug],
+            :parent => page[:parent]
+          }
         end
+
+        page_name_list
+      end
+
+
+      def self.write_posts(posts)
+        posts.each do |post|
+
+          filename = post[:filename]
+
+          FileUtils.mkdir_p(File.dirname(filename))
+
+          # Write out the data and content to file
+          File.open(filename, "w") do |f|
+            f.puts post[:frontmatter].to_yaml
+            f.puts "---"
+            f.puts Util.wpautop(post[:content])
+          end
+        end
+      end
+
+
+      def self.clean_entities(text)
+        text.force_encoding("UTF-8") if text.respond_to?(:force_encoding)
         text = HTMLEntities.new.encode(text, :named)
         # We don't want to convert these, it would break all
         # HTML tags in the post and comments.
@@ -351,11 +405,11 @@ module JekyllImport
       end
 
 
-      def self.sluggify( title )
+      def self.sluggify(title)
         title = title.to_ascii.downcase.gsub(/[^0-9A-Za-z]+/, " ").strip.gsub(" ", "-")
       end
 
-      def self.page_path( page_id, page_name_list )
+      def self.page_path(page_id, page_name_list)
         if page_name_list.key?(page_id)
           [
             page_path(page_name_list[page_id][:parent],page_name_list),
